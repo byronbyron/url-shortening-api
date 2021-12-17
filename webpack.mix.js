f088500562b2814be551ef287baee186a3b6827e
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-12-17 11:35:35 +0000

    Add styles for form on mobile and desktop

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,7 @@ let mix = require('laravel-mix');
 mix
     .js('src/js/app.js', 'js')
     .sass('src/scss/app.scss', 'css')
-    .sourceMaps(false, 'source-map');
+    .sourceMaps(false, 'source-map')
+    .options({
+        processCssUrls: false
+    });
